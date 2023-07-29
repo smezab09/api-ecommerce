fb7a304d621d813f8e4a8f48476237db89c8a25f
Sebastian Meza <sebasmezab@gmail.com>
2023-07-29 00:01:03 -0500

    corrección

--- src/database/config.js
+++ src/database/config.js
@@ -1,22 +1,17 @@
 const mongoose = require("mongoose");
 
 const dbConnection = () => {
-  mongoose.connect(process.env.MONGODB_CNN, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true, // opción para evitar el deprecated warning
-  });
+  try {
+    mongoose.connect(process.env.MONGODB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  const db = mongoose.connection;
-
-  db.on("error", (error) => {
-    console.error("Error de conexión a la base de datos:", error);
-    throw new Error("Error al iniciar la base de datos");
-  });
-
-  db.once("open", () => {
     console.log("Base de datos online");
-  });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error al iniciar la base de datos");
+  }
 };
 
 module.exports = {
