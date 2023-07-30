5d144df38a0ca045ee7aad6837dcdad0584d3b15
Sebastian Meza <sebasmezab@gmail.com>
2023-07-30 18:50:37 -0500

    token

--- src/index.js
+++ src/index.js
@@ -8,15 +8,21 @@ dbConnection();
 app.use(cors());
 app.use(express.json());
 
+// Middleware para verificar el token de autenticación
+const validarJWT = require("./middleware/jwt.middleware");
+
 app.get("/api", (req, res) => {
   return res.json({
     msg: "Bienvenido al Api de mi E-Commerce",
   });
 });
 
+// Rutas públicas que no requieren autenticación
 app.use("/api/usuarios", require("./routes/users.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/productos", require("./routes/products.routes"));
+
+// Rutas protegidas que requieren autenticación
+app.use("/api/productos", validarJWT, require("./routes/products.routes"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
